1e860c4ca9da3547c4d0cfafb042921365de62a4
Denis <7602501@gmail>
2021-10-10 12:56:02 +0300

    CommentDAO version 1.0

---
+++ src/main/java/by/c43/store/dao/CommentDao.java
@@ -0,0 +1,23 @@
+package by.c43.store.dao;
+
+import by.c43.store.entity.Comment;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface CommentDao {
+
+    void save(Comment comment);
+
+    void update(long id, String newDescription);
+
+    void delete(long id);
+
+    Optional<Comment> getById(long id);
+
+    boolean isExistById(long id);
+
+    boolean isExistByInfo(Comment comment);
+
+
+}

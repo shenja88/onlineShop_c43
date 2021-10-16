960432cc7c928869cce1ea81b889eabcd0b63857
Evgeniy2312 <evgen.matvee1986@gmail.com>
2021-10-16 21:23:01 +0300

    add interceptor, aspect(user), ExceptionController and refactoring controllers

--- src/main/java/by/c43/store/controller/ProductController.java
+++ src/main/java/by/c43/store/controller/ProductController.java
@@ -152,9 +152,9 @@ public class ProductController {
     public String updateRating(@PathVariable long id, double newPrice, Model model, HttpSession session){
         User user = (User) session.getAttribute("user");
         if(productService.updateScore(newPrice, id, user)){
-            model.addAttribute("messageUpdRating1", "Rating has been changed!");
+            model.addAttribute("messageUpdRating1", "Rating changed");
         } else{
-            model.addAttribute("messageUpdRating", "Operation failed!");
+            model.addAttribute("messageUpdRating2", "Failed!");
         }
         return "updRatingProd";
     }
